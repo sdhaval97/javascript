bd7a17ff63edaa057a7fac520156eed1e25a7db9
sdhaval97 <sdhaval97@gmail.com>
2025-07-13 23:59:03 +0530

    updated the javascript file

---
+++ Simple Mood Journal/script.js
@@ -0,0 +1,38 @@
+// I. Selecting the form elements
+// Form
+const form = document.getElementById(`journal-form`);
+
+// Text Area
+const entryText = document.getElementById(`entry-text`);
+
+// Mood Select
+const moodSelect = document.getElementById(`mood`);
+
+// Entry list container
+const entryList = document.getElementById(`entry-list`);
+
+console.log(form, entryText, moodSelect, entryList);
+
+const entries = []; // array to store journal entries
+
+// II. Event Listener when form is submitted
+form.addEventListener("submit", function (event){
+    event.preventDefault(); // stops form from submitting the traditional way
+
+    // Get the user input and the mood
+    const journal = entryText.value.trim();
+    const mood = moodSelect.value;
+
+    // Creating an object for new entry
+    const newEntry = {
+        text: journal,
+        mood: mood,
+        date: new Date().toLocaleString()
+    };
+
+    // pushing the new entry to the entries array
+    entries.push(newEntry);
+    console.log(entries);
+
+});
+
