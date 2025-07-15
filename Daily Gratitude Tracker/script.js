005346b9226e3d5cbb749bb5e2d148d25c302404
sdhaval97 <sdhaval97@gmail.com>
2025-07-16 00:15:03 +0530

    updated the js file

--- Daily Gratitude Tracker/script.js
+++ Daily Gratitude Tracker/script.js
@@ -4,6 +4,9 @@ const form = document.getElementById("gratitude-form");
 const gratitude = document.getElementById("gratitude-input");
 const entryList = document.getElementById("entry-list");
 
+// array to hold gratitude entries
+const gratitudeEntries = [];
+
 // Event listener to handle form submissions
 form.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -13,4 +16,12 @@ form.addEventListener("submit", function(event) {
 
     //clearing the text area
     gratitude.value = "";
-})
+
+    const newEntry = {
+        text: value,
+        date: new Date().toLocaleDateString("en-IN", { day: "numeric", month: "long", year: "numeric" })
+    };
+
+    gratitudeEntries.push(newEntry);
+    console.log(gratitudeEntries);
+});
